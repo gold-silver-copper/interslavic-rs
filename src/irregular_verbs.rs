cc2bf00ced0a8f9d65f294ed672a75cf9f672875
zombkit <stephen.korzen@gmail.com>
2024-10-18 10:32:52 -0700

    uj

--- src/irregular_verbs.rs
+++ src/irregular_verbs.rs
@@ -1,7 +1,7 @@
 pub const PREFIXES: &[&str] = &[
-    "prědpo", "razpro", "råzpro", "izpo", "odpo", "nad", "pod", "pre", "pri", "pro", "prě", "raz",
-    "råz", "voz", "vȯz", "do", "iz", "na", "ne", "ob", "od", "po", "sȯ", "vo", "vy", "vȯ", "za",
-    "o", "s", "u", "v", "",
+    "", "v", "u", "s", "o", "za", "vȯ", "vy", "vo", "sȯ", "po", "od", "ob", "ne", "na", "iz", "do",
+    "vȯz", "voz", "råz", "raz", "prě", "pro", "pri", "pre", "pod", "nad", "odpo", "izpo", "råzpro",
+    "razpro", "prědpo",
 ];
 
 pub const IRREGULAR_STEMS: &[(&str, &str)] = &[
